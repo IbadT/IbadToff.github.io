86a27026c53183b7cada2ae5a53a45a74a699a2b
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2022-05-14 00:31:51 +0300

    Add files via upload

---
+++ Main/indexForForm.php
@@ -0,0 +1,8 @@
+<?php
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $someInfo = $_POST['someInfo'];
+    
+    echo 'name: '.$name.'<br/>'.'phone: '.$phone.'<br/>'.'email: '.$email.'<br/>'.'someInfo: '.$someInfo;
+?>
---
+++ Main/php/index.php
@@ -0,0 +1,5 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    echo 'name: '.$name.'<br/>'.'email: '.$email;
+?>
